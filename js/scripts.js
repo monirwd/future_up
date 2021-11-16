f6cfd902f17090cd3047756d1ee94a0a540c8c05
monirwd <monirwd007@gmail.com>
2021-11-17 01:23:20 +0600

    banner-area

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-(function($){
-	jQuery(document).ready(function() {	
+(function ($) {
+	jQuery(document).ready(function () {
 
 		// Scroll to Top
-		jQuery('.scrollto-top').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery('.scrollto-top').click(function () {
+			jQuery('html').animate({ 'scrollTop': '0px' }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
+			if (upto > 500) {
 				jQuery('.scrollto-top').fadeIn();
 			} else {
 				jQuery('.scrollto-top').fadeOut();
@@ -17,29 +17,37 @@
 		});
 
 		//header-fixed
-		jQuery(window).scroll(function() {
+		jQuery(window).scroll(function () {
 			var scroll = jQuery(window).scrollTop();
 			if (scroll >= 15) {
-			jQuery(".header-area").addClass("headerfixed");
-			jQuery(".navbar-brand img").addClass("logo-width")
+				jQuery(".header-area").addClass("headerfixed");
+				jQuery(".navbar-brand img").addClass("logo-width")
 			} else {
-			jQuery(".header-area").removeClass("headerfixed");
-			jQuery(".navbar-brand img").removeClass("logo-width")
+				jQuery(".header-area").removeClass("headerfixed");
+				jQuery(".navbar-brand img").removeClass("logo-width")
 			}
 
 		});
 
+		//videp-modal script
+		jQuery(".js-video-button").modalVideo({
+			youtube: {
+				controls: 0,
+				nocookie: true
+			}
+		});
 
-		
 
-				
-		
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
+
+
+
+
 	});
 })(jQuery);
