d332811f47ce91179042e57fa0c5d55470526864
monirwd <monirwd007@gmail.com>
2021-11-16 17:36:38 +0600

    header-area

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+(function($){
+	jQuery(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrollto-top').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrollto-top').fadeIn();
+			} else {
+				jQuery('.scrollto-top').fadeOut();
+			}
+		});
+
+		//header-fixed
+		jQuery(window).scroll(function() {
+			var scroll = jQuery(window).scrollTop();
+			if (scroll >= 15) {
+			jQuery(".header-area").addClass("headerfixed");
+			jQuery(".navbar-brand img").addClass("logo-width")
+			} else {
+			jQuery(".header-area").removeClass("headerfixed");
+			jQuery(".navbar-brand img").removeClass("logo-width")
+			}
+
+		});
+
+
+		
+
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery);
